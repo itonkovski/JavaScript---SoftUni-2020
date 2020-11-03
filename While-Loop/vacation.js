88209908757fb09bff4ff9dc1748088dcf9e4c99
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2020-11-03 17:18:31 +0100

    VacationCalc
    post in

---
+++ While-Loop/vacation.js
@@ -0,0 +1,46 @@
+function calculationsVacation(input) {
+    let index = 0;
+    let daysCount = 0;
+    let moneyNeeded = Number(input[index++]);
+    let availableMoney = Number(input[index++]);
+    let spendCounter = 0;
+
+    while(availableMoney < moneyNeeded) {
+        let action = input[index++];
+        let currentAmount = input[index++];
+        daysCount++;
+        if (action === "spend") {
+            if (availableMoney < currentAmount) {
+                availableMoney = 0;
+            }
+            else {
+                availableMoney -= currentAmount;
+            }
+            spendCounter++;
+            if (spendCounter === 5) {
+                break;
+            }
+        }
+        else {
+            spendCounter = 0;
+            availableMoney += currentAmount;
+        }
+    }
+    if (spendCounter === 5) {
+        console.log("You can't save the money.");
+        console.log(`${daysCount}`);
+    }
+    else {
+        console.log(`You saved the money for ${daysCount} days.`);
+    }
+}
+calculationsVacation(["250",
+"150",
+"spend",
+"50",
+"spend",
+"50",
+"save",
+"100",
+"save",
+"100"]);
