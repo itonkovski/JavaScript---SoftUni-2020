057ab6a39077d5cad819234f6a6bf2d52489f6c6
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2020-11-03 14:41:08 +0100

    OldBooks
    Finalise

---
+++ While-Loop/oldBooks.js
@@ -0,0 +1,26 @@
+function oldBooks(input) {
+    let favBook = input[0];
+    let index = 1;
+    let bookIsFound = false;
+    let nextBook = input[1];
+
+    while (nextBook !== "No More Books") {
+        if (nextBook === favBook) {
+            bookIsFound = true;
+            break;
+        }
+        index++;
+        nextBook = input[index];
+    }
+    if ( bookIsFound === false) {
+        console.log("The book you search is not here!");
+        console.log(`You checked ${index - 1} books.`)
+    }
+    else {
+        console.log(`You checked ${index - 1} books and found it.`);
+    }
+}
+oldBooks(["Troy",
+"Stronger",
+"Life Style",
+"Troy"]);
