de02af7de13d64ae862d1853c6e5cd601d157e33
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2020-11-11 13:33:34 +0100

    EqualSum
    Upload

---
+++ NestedLoops/equalSumsEqualOddPositions.js
@@ -0,0 +1,25 @@
+function solve(input) {
+    let firstNum = input[0];
+    let secondNum = input[1];
+    let printLine = '';
+    for (let i = firstNum; i <= secondNum; i++) {
+        let currentNum = "" + i;
+        let oddSum = 0;
+        let evenSum = 0;
+        for (let j = 0; j <= currentNum.length; j++) {
+            let currentDigit = Number(currentNum.charAt(j));
+            if (j % 2 === 0) {
+                evenSum += currentDigit;
+            }
+            else {
+                oddSum += currentDigit;
+            }
+        }
+        if (oddSum === evenSum) {
+            printLine += `${i} `;
+        }
+    }
+    console.log(printLine);
+}
+solve(["123456",
+"124000"]);
